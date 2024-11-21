d2f51ceea9226a3fc2ac5cbed0f9c49c7f234e46
sabrinaachille <sabrinaachille1@gmail.com>
2024-11-21 01:41:39 -0500

    - styling update
    Sabrina Achille

--- src/main/java/com/example/csc325capstone/View/NewPostController.java
+++ src/main/java/com/example/csc325capstone/View/NewPostController.java
@@ -6,11 +6,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class NewPostController {
+    @FXML
+    private AnchorPane postAnchor;
+    @FXML
+    private Pane postPane;
     @FXML
     private Button activityBTN;
     @FXML
