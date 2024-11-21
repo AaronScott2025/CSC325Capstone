e90c99f9b7430403eed6ea810ad586dbe480aa06
sabrinaachille <sabrinaachille1@gmail.com>
2024-11-20 23:30:31 -0500

    - update main fxml > splitpane
    Sabrina Achille

--- src/main/java/com/example/csc325capstone/View/MainController.java
+++ src/main/java/com/example/csc325capstone/View/MainController.java
@@ -7,16 +7,22 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 public class MainController {
+    @FXML
+    private AnchorPane bottomAnchor;
+
+    @FXML
+    private AnchorPane topAnchor;
+
+    @FXML
+    private SplitPane splitPane;
 
     @FXML
     private Button activityBTN;
